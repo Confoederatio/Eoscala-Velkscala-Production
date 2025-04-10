807f29ae8ededaf6d206bb4fc41051077b207b11
Confoederatio <htmlprogrammingbn@gmail.com>
2025-04-10 01:16:01 +0100

    refactored OLS framework

--- core/framework/eoscala/potential_economic_activity_framework.js
+++ core/framework/eoscala/potential_economic_activity_framework.js
@@ -1,77 +1,9 @@
-//Initialise adjustment functions
-{
-  global.computeVIF = function (arg0_X) {
-    //Convert from parameters
-    var X = arg0_X;
-
-    //Declare local instance variables
-    var XT_X = mathjs.multiply(mathjs.transpose(X), X);
-    var XT_X_inv = mathjs.inv(XT_X);
-    var vif = XT_X_inv.map((row, i) => row[i]);
-
-    //Return statement
-    return vif;
-  };
-
-  global.conditionNumber = function (arg0_X, arg1_epsilon) {
-    //Convert from parameters
-    var X = arg0_X;
-    var epsilon = returnSafeNumber(arg1_epsilon, 1e-12);
-
-    //Declare local instance variables
-    try {
-      X = X._data;
-    } catch (e) {}
-
-    var matrix = new ml_matrix.SVD(X, { autoTranspose: true });
-    var singular_values = matrix.diagonal;
-
-    //Find max and min singular values
-    var max_s = Math.max(...singular_values);
-    var min_s = Math.max(Math.min(...singular_values), epsilon); //Ensure min_s is never 0
-
-    //Return statement
-    return max_s/min_s;
-  };
-
-  global.removeHighVIFFeatures = function (arg0_X, arg1_threshold) {
-    //Convert from parameters
-    var X = arg0_X;
-    var threshold = returnSafeNumber(arg1_threshold, 10);
-
-    //Declare local instance variables
-    var vif_scores = computeVIF(X);
-    console.log(`- Computed VIF scores.`);
-    var to_keep = vif_scores.map((vif, i) => (vif < threshold));
-    console.log(`- Found VIF scores to keep.`);
-
-    //Return statement
-    return X.map((row) => row.filter((_, index) => to_keep[index]));
-  };
-
-  global.ridgeRegression = function (arg0_X, arg1_Y, arg2_lambda) {
-    //Convert from parameters
-    var X = arg0_X;
-    var Y = arg1_Y;
-    var lambda = returnSafeNumber(arg2_lambda, 1e-3);
-
-    //Declare local instance variables
-    var XT = mathjs.transpose(X);
-    var XT_X = mathjs.multiply(XT, X);
-    var identity = mathjs.identity(XT_X.size()[0]);
-
-    var XT_X_reg = mathjs.add(XT_X, mathjs.multiply(identity, lambda)); //Ridge term
-    var XT_Y = mathjs.multiply(XT, Y);
-
-    //Return statement; return beta
-    return mathjs.multiply(mathjs.inv(XT_X_reg), XT_Y);
-  };
-}
-
 //Initialise functions
 {
-  //calculateFirstStepIntermediateModelGeomean() - Calculates intermediate model geomeans for HYDE-SEDAC. Not the processed SEDAC-adjusted model
-  global.calculateFirstStepIntermediateModelGeomean = function (arg0_prefix) {
+  /**
+   * calculatePotentialEconomicActivityGeomean() - Calculates intermediate model geomeans for HYDE-SEDAC. Not the processed SEDAC-adjusted model
+  **/
+  global.calculatePotentialEconomicActivityGeomean = function (arg0_prefix) {
     //Convert from parameters
     var prefix = (arg0_prefix) ? arg0_prefix : `base_model_`;
 
@@ -107,9 +39,9 @@
         }
       }
 
-    //Compute geometric mean for each coefficient
+    //Iterate over all coefficients; Compute geometric mean for each coefficient
     var hybrid_coefficients = {};
-
+    
     for (var key in all_coefficients)
       hybrid_coefficients[key] = weightedGeometricMean(all_coefficients[key]);
 
@@ -152,10 +84,10 @@
   };
 
   /*
-    trainFirstStepIntermediateModel() - Trains adjusted, stabler HYDE-SEDAC OLS model with full dynamic lambda Ridge Regression.
+    trainPotentialEconomicActivityIntermediateModel() - Trains adjusted, stabler HYDE-SEDAC OLS model with full dynamic lambda Ridge Regression.
     arg0_year: (Number)
   */
-  global.trainFirstStepIntermediateModel = async function (arg0_year) {
+  global.trainPotentialEconomicActivityIntermediateModel = async function (arg0_year) {
     //Convert from parameters
     var year = parseInt(arg0_year);
 
@@ -203,10 +135,10 @@
   };
 
   /*
-    trainFirstStepProcessedModel() - Trains the adjusted processed model by iterating over all HYDE-SEDAC years and finding the weighted geomean global scalar for all coefficients
+    trainPotentialEconomicActivityProcessedModel() - Trains the adjusted processed model by iterating over all HYDE-SEDAC years and finding the weighted geomean global scalar for all coefficients
     arg0_year: (Number)
   */
-  global.trainFirstStepProcessedModel = async function (arg0_options) {
+  global.trainPotentialEconomicActivityProcessedModel = async function (arg0_options) {
     //Convert from parameters
     var options = (arg0_options) ? arg0_options : {};
 
@@ -214,7 +146,7 @@
     if (!main.models.hyde_sedac) main.models.hyde_sedac = {};
       if (!main.models.hyde_sedac.processed_model) main.models.hyde_sedac.processed_model = {};
     try {
-      calculateFirstStepIntermediateModelGeomean(`base_adjusted_model_`)
+      calculatePotentialEconomicActivityGeomean(`base_adjusted_model_`)
     } catch (e) { console.error(e); }
 
     //Declare local instance variables
@@ -306,7 +238,7 @@
 
       console.log(`- New coefficients:`, processed_model.coefficients);
     } catch (e) {
-      console.log(`trainFirstStepProcessedModel(): Error when parsing ${main.sedac_domain[0] + i}:`);
+      console.log(`trainPotentialEconomicActivityProcessedModel(): Error when parsing ${main.sedac_domain[0] + i}:`);
       console.error(e);
     }
 
@@ -319,10 +251,10 @@
   };
 
   /*
-    trainFirstStepRawModel() - Trains a baseline HYDE-SEDAC OLS model. Very unstable, but optimised for hybridisation and geometric mean.
+    trainPotentialEconomicActivityRawModel() - Trains a baseline HYDE-SEDAC OLS model. Very unstable, but optimised for hybridisation and geometric mean.
     arg0_year: (Number)
   */
-  global.trainFirstStepRawModel = async function (arg0_year) {
+  global.trainPotentialEconomicActivityRawModel = async function (arg0_year) {
     //Convert from parameters
     var year = parseInt(arg0_year);
 
@@ -357,21 +289,21 @@
     console.log(`Model data for ${year} saved successfully in ${output_file_path}.`);
   };
 
-  global.trainFirstStepRawModels = async function () {
+  global.trainPotentialEconomicActivityRawModels = async function () {
     //Declare local instance variables
     var sedac_years = main.sedac_domain[1] - main.sedac_domain[0];
 
     //Iterate over all sedac_years; include end year
     for (var i = 0; i < sedac_years + 1; i++)
-      await trainFirstStepRawModel(main.sedac_domain[0] + i);
+      await trainPotentialEconomicActivityRawModel(main.sedac_domain[0] + i);
   };
 
-  global.trainFirstStepIntermediateModels = async function () {
+  global.trainPotentialEconomicActivityIntermediateModels = async function () {
     //Declare local instance variables
     var sedac_years = main.sedac_domain[1] - main.sedac_domain[0];
 
     //Iterate over all sedac_years; include end year
     for (var i = 0; i < sedac_years + 1; i++)
-      await trainFirstStepIntermediateModel(main.sedac_domain[0] + i);
+      await trainPotentialEconomicActivityIntermediateModel(main.sedac_domain[0] + i);
   };
 }
