752bd975665335808672d5c8a18d18f9c93b4983
Confoederatio <htmlprogrammingbn@gmail.com>
2025-04-10 02:03:49 +0100

    Update potential_economic_activity_framework.js

--- core/framework/eoscala/potential_economic_activity_framework.js
+++ core/framework/eoscala/potential_economic_activity_framework.js
@@ -2,6 +2,9 @@
 {
   /**
    * calculatePotentialEconomicActivityGeomean() - Calculates intermediate model geomeans for HYDE-SEDAC. Not the processed SEDAC-adjusted model
+   * @param {String} arg0_prefix - The prefix to use for saving base model output files.
+   * 
+   * @returns {Object}
   **/
   global.calculatePotentialEconomicActivityGeomean = function (arg0_prefix) {
     //Convert from parameters
@@ -49,13 +52,18 @@
     var output_path = `${config.defines.common.output_file_paths.OLS_potential_economic_activity_folder}geomean_${prefix.split('_').join(" ").trim().split(" ").join("_")}.json`;
 
     fs.writeFileSync(output_path, JSON.stringify(output_data, null, 2));
-    console.log(`HYDE-SEDAC weighted geometric mean calculated and saved at ${output_path}.`);
+    log.info(`HYDE-SEDAC weighted geometric mean calculated and saved at ${output_path}.`);
+
+    //Return statement
+    return output_data;
   };
 
-  /*
+  /**
     loadHYDESEDACYear() - Loads HYDE-SEDAC data from raster files in ./output/ and uses them for OLS training
-    arg0_year: (Number)
-  */
+    @param {number} arg0_year - The year to load HYDE-SEDAC data for.
+
+    @returns {X: number, Y: number}
+  **/
   global.loadHYDESEDACYear = async function (arg0_year) {
       //Convert from parameters
       var year = parseInt(arg0_year);
@@ -65,11 +73,11 @@
       var hyde_dictionary = config.velkscala.hyde.hyde_dictionary;
       var sedac_data = loadNumberRasterImage(`${config.defines.common.output_file_prefix}${year}_number.png`).data;
 
-      //Load each HYDE variable as a predictor
+      //Iterate over all hyde stocks; load each HYDE variable as a predictor
       var all_hyde_keys = Object.keys(hyde_dictionary);
 
       for (var key of all_hyde_keys) {
-        var local_file_path = `output/HYDE_png/${key}${getHYDEYearName(year)}_number.png`;
+        var local_file_path = `${config.defines.common.output_file_paths.hyde_folder}${key}${getHYDEYearName(year)}_number.png`;
         var local_rawdata = loadNumberRasterImage(local_file_path).data;
 
         hyde_data.push(local_rawdata);
@@ -83,13 +91,17 @@
       return { X, Y };
   };
 
-  /*
-    trainPotentialEconomicActivityIntermediateModel() - Trains adjusted, stabler HYDE-SEDAC OLS model with full dynamic lambda Ridge Regression.
-    arg0_year: (Number)
-  */
-  global.trainPotentialEconomicActivityIntermediateModel = async function (arg0_year) {
+  /**
+    trainPotentialEconomicActivityModel() - Trains adjusted, stabler HYDE-SEDAC OLS model with full dynamic lambda Ridge Regression.
+    @param {number} arg0_year - The year to use for training the model.
+    @param {Object} [arg1_options]
+     @param {boolean} [arg1_options.remove_high_vif_features=false] - Whether to remove high VIF features.
+     @param {boolean} [arg1_options.dynamic_lambda=false] - Whether to dynamically select lambda based on condition number for Ridge Regression purposes.
+  **/
+  global.trainPotentialEconomicActivityModel = async function (arg0_year, arg1_options) {
     //Convert from parameters
     var year = parseInt(arg0_year);
+    var options = (arg1_options) ? arg1_options : {};
 
     //Declare local instance variables
     var hyde_dictionary = config.velkscala.hyde.hyde_dictionary;
@@ -97,31 +109,36 @@
 
     var all_hyde_keys = Object.keys(hyde_dictionary);
 
-    console.log(`Performing OLS for ${year} ..`);
+    log.info(`Performing OLS for ${year} ..`);
 
-    //1. Remove multicolinear features (DO NOT REMOVE - GEOMEAN SHOULD FIX EVERYTHING)
-    //X = removeHighVIFFeatures(X, 10);
-    //console.log(`- Removed High VIF Features.`);
+    //1. Remove multicolinear features using VIF selection if possible
+    if (options.remove_high_vif_features) {
+      X = removeHighVIFFeatures(X, 10);
+      log.info(`- Removed High VIF Features.`);
+    }
 
     //2. Apply Ridge Regression to stabilise coefficients
+    var selected_lambda = 1e9; //The condition_number is always so large that it is always 1e9 anyway
     var X_matrix = mathjs.matrix(X);
     var Y_matrix = mathjs.matrix(Y);
-    console.log(`- Computed preliminary matrices.`);
-    var condition_number = conditionNumber(X_matrix);
-    var selected_lambda = 1e9; //The condition_number is always so large that it is always 1e9 anyway
-      if (condition_number > 1e6) selected_lambda = 1e9;
-      else if (condition_number > 1e4) selected_lambda = 1e7;
-      else if (condition_number > 1e2) selected_lambda = 1e5;
-      else selected_lambda = 1e3; //Minimum regularisation
+    log.info(`- Computed preliminary matrices.`);
 
-    console.log(`- Condition Number: ${condition_number}, using Lambda = ${selected_lambda}`);
+    if (options.dynamic_lambda) {
+      var condition_number = conditionNumber(X_matrix);
+      if (condition_number > 1e6) selected_lambda = 1e9;
+        else if (condition_number > 1e4) selected_lambda = 1e7;
+        else if (condition_number > 1e2) selected_lambda = 1e5;
+        else selected_lambda = 1e3; //Minimum regularisation
+    }
+
+    log.info(`- Condition Number: ${condition_number}, using Lambda = ${selected_lambda}`);
 
     var beta = ridgeRegression(X_matrix, Y_matrix, selected_lambda); //Small lambda for stabilisation
-    console.log(`- Applied Ridge Regression to stabilise coefficients.`);
+    log.info(`- Applied Ridge Regression to stabilise coefficients.`);
 
     //3. Convert coefficients to JSON
     var coefficients = beta.toArray().flat();
-    console.log(`- Computed coefficients.`);
+    log.info(`- Computed coefficients.`);
 
     //Save model to JSON
     var model_data_obj = {
@@ -131,13 +148,14 @@
     var output_file_path = `${config.defines.common.output_file_paths.OLS_potential_economic_activity_folder}base_adjusted_model_${year}.json`;
 
     fs.writeFileSync(output_file_path, JSON.stringify(model_data_obj, null, 2));
-    console.log(`Adjusted model data for ${year} saved successfully in ${output_file_path}.`);
+    log.info(`Adjusted model data for ${year} saved successfully in ${output_file_path}.`);
   };
 
-  /*
+  /**
     trainPotentialEconomicActivityProcessedModel() - Trains the adjusted processed model by iterating over all HYDE-SEDAC years and finding the weighted geomean global scalar for all coefficients
-    arg0_year: (Number)
-  */
+    @param {Object} [arg0_options]
+     @param {boolean} [arg0_options.debug=false] - Whether to print debug information.
+  **/
   global.trainPotentialEconomicActivityProcessedModel = async function (arg0_options) {
     //Convert from parameters
     var options = (arg0_options) ? arg0_options : {};
@@ -165,14 +183,14 @@
     //Iterate over all SEDAC files
     for (var i = 0; i <= sedac_years; i++) try {
       var local_year = main.sedac_domain[0] + i;
-      console.log(`Processing HYDE-SEDAC geomean base adjusted model for ${local_year} ..`);
+      log.info(`Processing HYDE-SEDAC geomean base adjusted model for ${local_year} ..`);
 
       var local_file_path = `${config.defines.common.output_file_paths.sedac_prefix}${local_year}_number.png`;
       var local_hyde_images = {}; //Maps image data to hyde stocks
       var total_logs = {};
       var valid_hyde_keys = [];
       all_hyde_keys.forEach((key) => {
-        console.log(`- Processing HYDE key: ${key} ..`);
+        log.info(`- Processing HYDE key: ${key} ..`);
         try {
           local_hyde_images[key] = loadNumberRasterImage(`${config.defines.common.output_file_paths.hyde_folder}${key}${getHYDEYearName(local_year)}_number.png`);;
           valid_hyde_keys.push(key);
@@ -189,7 +207,7 @@
       }
 
       //Iterate over all pixels
-      console.log(`- Processing weights (bidirectional weighted average adjustment) ..`)
+      log.info(`- Processing weights (bidirectional weighted average adjustment) ..`)
       for (var x = 0; x < local_sedac_image.width*local_sedac_image.height; x++) {
         //Compute predicted_value based on HYDE stocks
         var predicted_value = 0;
@@ -208,7 +226,7 @@
 
           if (options.debug)
             if (hyde_value > 0 && returnSafeNumber(total_logs[key]) < 100)
-              console.log(`- HYDE: Pixel ${x}: ${key}: Hyde value: ${hyde_value}, Coefficient: ${coefficient}, Weighted contribution: ${weighted_contribution}`);
+              log.info(`- HYDE: Pixel ${x}: ${key}: Hyde value: ${hyde_value}, Coefficient: ${coefficient}, Weighted contribution: ${weighted_contribution}`);
         });
 
         var observed_value = returnSafeNumber(local_sedac_image.data[x], 0);
@@ -229,81 +247,32 @@
               if (options.debug)
                 if (returnSafeNumber(total_logs[key]) < 100) {
                   modifyValue(total_logs, key, 1);
-                  console.log(`- SEDAC Adj: Pixel ${x}: ${key}, Update Amount: ${update_amount}, Weight Fraction: ${weight_fraction}, Residual: ${residual}, Correction Factor: ${correction_factor}`);
+                  log.info(`- SEDAC Adj: Pixel ${x}: ${key}, Update Amount: ${update_amount}, Weight Fraction: ${weight_fraction}, Residual: ${residual}, Correction Factor: ${correction_factor}`);
                 }
               processed_model.coefficients[key] += returnSafeNumber(update_amount); //Apply the correction weighted by category presence
             }
           });
       }
 
-      console.log(`- New coefficients:`, processed_model.coefficients);
+      log.info(`- New coefficients:`, processed_model.coefficients);
     } catch (e) {
-      console.log(`trainPotentialEconomicActivityProcessedModel(): Error when parsing ${main.sedac_domain[0] + i}:`);
+      log.error(`trainPotentialEconomicActivityProcessedModel(): Error when parsing ${main.sedac_domain[0] + i}:`);
       console.error(e);
     }
 
     //Save adjusted coefficients
     main.models.hyde_sedac.processed_model = processed_model;
 
-    console.log(``)
     var output_file_path = `${config.defines.common.output_file_paths.OLS_potential_economic_activity_folder}processed_base_model.json`;
     fs.writeFileSync(output_file_path, JSON.stringify(processed_model, null, 2));
   };
 
-  /*
-    trainPotentialEconomicActivityRawModel() - Trains a baseline HYDE-SEDAC OLS model. Very unstable, but optimised for hybridisation and geometric mean.
-    arg0_year: (Number)
-  */
-  global.trainPotentialEconomicActivityRawModel = async function (arg0_year) {
-    //Convert from parameters
-    var year = parseInt(arg0_year);
-
-    //Declare local instance variables
-    var hyde_dictionary = config.velkscala.hyde.hyde_dictionary;
-    var { X, Y } = await loadHYDESEDACYear(year);
-
-    var all_hyde_keys = Object.keys(hyde_dictionary);
-
-    console.log(`Performing OLS for ${year} ..`);
-
-    //1. Apply Ridge Regression to stabilise coefficients
-    var X_matrix = mathjs.matrix(X);
-    var Y_matrix = mathjs.matrix(Y);
-    console.log(`- Computed preliminary matrices.`);
-
-    var beta = ridgeRegression(X_matrix, Y_matrix, 1e-3); //Small lambda for stabilisation
-    console.log(`- Applied Ridge Regression to stabilise coefficients.`);
-
-    //2. Convert coefficients to JSON
-    var coefficients = beta.toArray().flat();
-    console.log(`- Computed coefficients.`);
-
-    //Save model to JSON
-    var model_data_obj = {
-      year: year,
-      coefficients: Object.fromEntries(all_hyde_keys.map((key, i) => [key, coefficients[i]]))
-    };
-    var output_file_path = `${config.defines.common.output_file_paths.OLS_potential_economic_activity_folder}base_model_${year}.json`;
-
-    fs.writeFileSync(output_file_path, JSON.stringify(model_data_obj, null, 2));
-    console.log(`Model data for ${year} saved successfully in ${output_file_path}.`);
-  };
-
-  global.trainPotentialEconomicActivityRawModels = async function () {
+  global.trainPotentialEconomicActivityModels = async function () {
     //Declare local instance variables
     var sedac_years = main.sedac_domain[1] - main.sedac_domain[0];
 
     //Iterate over all sedac_years; include end year
     for (var i = 0; i < sedac_years + 1; i++)
-      await trainPotentialEconomicActivityRawModel(main.sedac_domain[0] + i);
-  };
-
-  global.trainPotentialEconomicActivityIntermediateModels = async function () {
-    //Declare local instance variables
-    var sedac_years = main.sedac_domain[1] - main.sedac_domain[0];
-
-    //Iterate over all sedac_years; include end year
-    for (var i = 0; i < sedac_years + 1; i++)
-      await trainPotentialEconomicActivityIntermediateModel(main.sedac_domain[0] + i);
+      await trainPotentialEconomicActivityModel(main.sedac_domain[0] + i);
   };
 }
