8541aa01bf44cb4e6a39a6f6be9c0f6226708be3
Confoederatio <htmlprogrammingbn@gmail.com>
2025-06-10 05:40:39 +0100

    added LUH2 and KK10 to internal inputs for a T0 toy model

---
+++ common/velkscala/luh2.js
@@ -0,0 +1,8 @@
+config.velkscala.luh2 = {
+	luh2_domain: [900, 2019],
+
+	file_prefix: "LUH2_",
+	input_folder: "./input/velkscala/LUH2/",
+	output_folder: "./input/velkscala/LUH2/anthropogenic_mean", //Folder from which all variables are seen as directories, including '/output_folder/' subfolder for processing
+	variables: ["c3ann", "c3nfx", "c3per", "c4ann", "c4per", "pasture", "urban"]
+};
---
+++ core/framework/velkscala/T0_substrata/kk10_luh2_framework.js
@@ -0,0 +1,14 @@
+//Initialise functions
+{
+	global.averageLUH2Rasters = function () {
+		//Declare local instance variables
+		var hyde_years = config.velkscala.hyde.hyde_years;
+		var luh2_config = config.velkscala.luh2;
+
+		//Iterate over all hyde_years
+		for (var i = luh2_config.luh2_domain[0]; i <= luh2_config.luh2_domain[1]; i++)
+			if (hyde_years.includes(i)) {
+
+			}
+	};
+}
