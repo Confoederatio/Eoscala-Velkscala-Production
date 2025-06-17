4fec1258a05849fe43686227d5e1a1fdc2df0957
Confoederatio <htmlprogrammingbn@gmail.com>
2025-06-17 08:18:08 +0100

    Update nelson.js

--- common/velkscala/nelson.js
+++ common/velkscala/nelson.js
@@ -1,3 +1,4 @@
 config.velkscala.nelson = {
-	nelson_domain: [-3000, 1900] //Far better datasets become available post-1900AD; superset for Velkscala should only be scaled for in-domain years, without interpolation
+	nelson_domain: [-3000, 1900], //Far better datasets become available post-1900AD; superset for Velkscala should only be scaled for in-domain years, without interpolation
+	nelson_years: [-3000, -2200, -1400, -700, -300, -100, 500, 800, 1000, 1200, 1300,  1400, 1500, 1600, 1650, 1700, 1750, 1800, 1820, 1840, 1860, 1880, 1900] //In-domain years for Nelson
 };
