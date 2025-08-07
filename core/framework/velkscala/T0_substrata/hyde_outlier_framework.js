8cf99ecc4510494250151e7185d2ac5f43a8bb1a
Confoederatio <htmlprogrammingbn@gmail.com>
2025-08-07 05:39:22 +0100

    began masking hyde outliers

--- core/framework/velkscala/T0_substrata/hyde_outlier_framework.js
+++ core/framework/velkscala/T0_substrata/hyde_outlier_framework.js
@@ -1,4 +1,14 @@
 //Initialise functions
 {
-
+	function getHYDEOutlierMasksObject () { //[WIP] - Finish function body
+	
+	}
+	
+	function removeOutliersForHYDE () { //[WIP] - Finish function body
+	
+	}
+	
+	function removeOutliersForHYDEYear (arg0_year) { //[WIP] - Finish function body
+	
+	}
 }
